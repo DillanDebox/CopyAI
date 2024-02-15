5d786ec24c724e0dc41b08430fc0c57399f788f2
Dillan Sievwright <dillan@deboxfinancial.com>
2024-02-15 17:30:03 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -21,10 +21,9 @@ def check_password():
     if st.session_state.get("password_correct", False):
         return True
 
-    # Show input for password.
+    st.header("Please enter the password below:")
     st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
-    )
+        "Password", type="password", on_change=password_entered, key="password")
     if "password_correct" in st.session_state:
         st.error("😕 Password incorrect")
     return False
